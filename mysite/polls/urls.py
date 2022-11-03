d503c9281b0472803d1ceb3effc1375dc9cf4a96
Enekwechi Chinonso Gerald <chiiinonsky0009@gmail.com>
2022-11-03 23:00:26 +0100

    update urls.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
+from django.views import View
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-  path('', views.index, name = 'index')
+  path('', views.index, name = 'index'),
+  path('funcky', views.funcky, name= 'cats'),
+  path('danger/<int:guess>', views.danger),
+  path('main/<int:guess>', views.MainView.as_view(), name= 'second-view'),
+  path('bounce', views.bounce, name='dogs'),
+  path('game/<slug:guess>', views.GameView.as_view(), name='dog'),
+  path('guess', views.guess),
+  path('loop', views.loop, name='first-view'),
+  path('second', views.SecondView.as_view()),
+  path('guesspost', views.ClassyView.as_view())
 ]
