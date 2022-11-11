af0719103d6f3d1ac2173045ea6b4d22d024cfd9
Enekwechi Chinonso Gerald <chiiinonsky0009@gmail.com>
2022-11-11 22:04:47 +0100

    update view

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from home.models import Article
+from home.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 # Create your views here.
+
+class ArticleListView(OwnerListView):
+  model = Article
+  # By convention:
+  # template_name = "myarts/article_list.html"
+
+
+class ArticleDetailView(OwnerDetailView):
+  model = Article
+
+class ArticleCreateView(OwnerCreateView):
+  model = Article
+  # List the fields to copy from the Article model to the Article form
+  fields = ['title', 'text']
+
+class ArticleUpdateView(OwnerUpdateView):
+  model = Article
+  fields = ['title', 'text']
+  # This would make more sense
+  # fields_exclude = ['owner', 'created_at', 'updated_at']
+
+
+class ArticleDeleteView(OwnerDeleteView):
+  model = Article
